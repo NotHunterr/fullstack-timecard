d12e3c298754514a20cf45153b0eb70af18060af
NotHunterr <notryoshi@gmail.com>
2023-03-23 20:26:03 -0700

    Front-end Client basics finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import { useState, useEffect } from 'react'
+import Axios from 'axios'
+import './styles.css'
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([])
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
+  const [username, setUsername] = useState('')
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    Axios.get('http://localhost:3500/getUsers').then((response) => {
+      setListOfUsers(response.data)
+    })
+  }, [])
+
+  const createUser = () => {
+    Axios.post('http://localhost:3500/createUser', {
+      name: name,
+      age: age,
+      username: username,
+    }).then((response) => {
+      alert('Success')
+    })
+  }
 
   return (
     <div className="App">
@@ -17,6 +36,30 @@ function App() {
             </div>
           )
         })}
+        <div>
+          <input
+            type="text"
+            placeholder="Name..."
+            onChange={(event) => {
+              setName(event.target.value)
+            }}
+          />
+          <input
+            type="number"
+            placeholder="Age..."
+            onChange={(event) => {
+              setAge(event.target.value)
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Username..."
+            onChange={(event) => {
+              setUsername(event.target.value)
+            }}
+          />
+          <button onClick={createUser}>Create User</button>
+        </div>
       </div>
     </div>
   )
